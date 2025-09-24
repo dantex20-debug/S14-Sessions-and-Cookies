73bc0bdae15c6d8e6355d8eeebdf0bd4ecd7c7de
WodaMineralna <raknieborak65@gmail.com>
2025-09-24 19:42:33 +0200

    feat(auth): add basic login routes with session and cookie handling
    - `loggedIn` session cookie is created after user submits login form

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 exports.getLogin = (req, res, next) => {
+  console.log("Session `loggedIn` cookie:", req.session.loggedIn); // DEBUGGING
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
   });
+};
+
+// ! real user authentication will be implemented during the next course
+exports.postLogin = (req, res, next) => {
+  req.session.loggedIn = true;
+  res.redirect("/");
 };
